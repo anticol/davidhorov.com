3978dba8120b027b4a11f5946e127698b8f829b6
David Horov <david.horov@primehammer.com>
2024-10-29 12:00:58 +0100

    fix lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (<>
     <Analytics />
-    <Component {...pageProps} />;
+    <Component {...pageProps} />
   </>
   )
 
