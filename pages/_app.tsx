8a4b73153aa8b7a272e90fb70205e26edb5daef2
David Horov <david.horov@primehammer.com>
2024-10-29 11:59:19 +0100

    feat: change the position of analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import "tailwindcss/tailwind.css";
 import "@/styles/main.css";
+import { Analytics } from '@vercel/analytics/react';
 
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return
+  <>
+    <Analytics />
+    <Component {...pageProps} />;
+
+  </>
+
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import { Analytics } from "@vercel/analytics/react"
 
 class MyDocument extends Document {
   render() {
@@ -9,7 +8,6 @@ class MyDocument extends Document {
           <link rel="icon" type="image/ico" href="/static/favicon/favicon.ico" />
         </Head>
         <body>
-          <Analytics />
           <Main />
           <NextScript />
         </body>
